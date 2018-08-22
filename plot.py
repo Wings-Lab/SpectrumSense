6bab0e486b4e58e4fb8c6ab82d49bfb4b19e8ef8
bershgal <matique@cs.stonybrook.edu>
2018-08-22 11:19:57 -0400

    Made changes to the plot script

--- plot.py
+++ plot.py
@@ -17,12 +17,14 @@ d5 = np.mean(d5, axis=0)
 x = [d1[0], d2[0], d3[0], d4[0], d5[0]]
 y = [d1[1], d2[1], d3[1], d4[1], d5[1]]
 z = [d1[2], d2[2], d3[2], d4[2], d5[2]]
+plt.xticks(np.arange(5), ('0.01-0.02', '0.03-0.05', '0.08-0.1', '0.13-0.15', '0.18-0.2'))
+trans_lenght =['0.01-0.02',0.05]
+plt.plot(x, linewidth = '3', marker = '*', markersize = 12, fontsize = 12)
+plt.plot(y, linewidth = '3', marker = '*', markersize = 12, fontsize = 12)
+plt.plot(z, linewidth = '3', marker = '*', markersize = 12, fontsize = 12)
 
-plt.plot(x)
-plt.plot(y)
-plt.plot(z)
-
-plt.ylabel("Detection Ratio")
-plt.xlabel("Transmission length")
+plt.ylabel("Detection Ratio", fontsize = 15)
+plt.xlabel("Transmission length", fontsize = 15)
+plt.title("Variarion of Detection Probability with Transmission Time", fontsize = 12)
 
 plt.show()
